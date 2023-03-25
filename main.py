e089ff7490622d499948e779ab015d15422120d1
Marlene <marlenetourn@gmail.com>
2023-03-25 12:04:19 -0300

    -cambia colores

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import turtle
 from math import cos,sin
 from time import sleep
+import random
 
 window = turtle.Screen()
 window.bgcolor("#FFFFFF")
@@ -10,7 +11,7 @@ myPen = turtle.Turtle()
 myPen.hideturtle()
 # myPen.tracer(0)
 myPen.speed(4)
-myPen.pensize(3)
+myPen.pensize(2)
 myPen.color("#AA00AA")
 
 myPen.penup()
@@ -22,11 +23,15 @@ b = 4
 delta = 3.14/2
 t=0
 
+colours = ['#b24a68', '#FAEBD7', '#F0FFFF', '#6495ED', '#008B8B', '#483D8B', '#20B2AA', '#9cb24a', '#abef57']
+
 for i in range(0,1000):
     t+=0.01
     #Apply Lissajous Parametric Equations
     x = A * sin(a*t + delta) 
     y = B * sin(b*t) 
+
+    myPen.color(random.choice(colours))
 
     myPen.goto(x,y)
     myPen.pendown()
