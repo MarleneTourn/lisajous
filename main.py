a7509243e4d7c048112b5ea5daca5d5802ca4fe2
Marlene <marlenetourn@gmail.com>
2023-03-25 12:44:28 -0300

    -refactorizo un poquitin

--- main.py
+++ main.py
@@ -4,50 +4,44 @@ from math import cos,sin
 from time import sleep
 import random
 
-A = int(input("Dígame el ancho del gráfico: "))
-B = int(input("Dígame el alto del gráfico: "))
-a = int(input("Dígame a: "))
-b = int(input("Dígame b: "))
-delta = float(input("Dígame delta mayor que 0 y menor o igual que 2: "))
-delta = 3.14/delta
-t = int(input("Dígame t: "))
 
-window = turtle.Screen()
-window.bgcolor("#006b9b")
+def ask_values():
+    A = int(input("Dígame el ancho del gráfico: "))
+    B = int(input("Dígame el alto del gráfico: "))
+    a = int(input("Dígame a: "))
+    b = int(input("Dígame b: "))
+    delta = float(input("Dígame delta mayor que 0 y menor o igual que 2: "))
+    delta = 3.14/delta
+    t = int(input("Dígame t: "))
+    return A, B, a, b, delta, t
 
-myPen = turtle.Turtle()
-myPen.hideturtle()
-# myPen.tracer(0)
-myPen.speed(4)
-myPen.pensize(2)
-myPen.color("#AA00AA")
+def draw_line(A, B, a, b, delta, t):
+    window = turtle.Screen()
+    window.bgcolor("#006b9b")
+    myPen = turtle.Turtle()
+    myPen.hideturtle()
+    myPen.speed(10)
+    myPen.pensize(2)
+    myPen.color("#AA00AA")
+    myPen.penup()
+    colours = ['#b24a68', '#FAEBD7', '#F0FFFF', '#6495ED', '#008B8B', '#483D8B', '#20B2AA', '#9cb24a', '#abef57']
 
-myPen.penup()
+    for i in range(0,1000):
+        t+=0.01
+        #Apply Lissajous Parametric Equations
+        x = A * sin(a*t + delta) 
+        y = B * sin(b*t) 
 
-# A = 100
-# B = 100
-# a = 3 
-# b = 4
-# delta = 3.14/2
-# t=0
+        if i % 5 == 0:
+            myPen.color(random.choice(colours))
 
-colours = ['#b24a68', '#FAEBD7', '#F0FFFF', '#6495ED', '#008B8B', '#483D8B', '#20B2AA', '#9cb24a', '#abef57']
+        myPen.goto(x,y)
+        myPen.pendown()
+        myPen.getscreen().update() 
+    sleep(0.5)
 
-for i in range(0,1000):
-    t+=0.01
-    #Apply Lissajous Parametric Equations
-    x = A * sin(a*t + delta) 
-    y = B * sin(b*t) 
 
-    if i % 5 == 0:
-        myPen.color(random.choice(colours))
+if __name__ == '__main__':
+    A, B, a, b, delta, t = ask_values()
+    draw_line(A, B, a, b, delta, t)
 
-    myPen.goto(x,y)
-    myPen.pendown()
-    myPen.getscreen().update() 
-
-sleep(0.5)
-
-  
-
-  
