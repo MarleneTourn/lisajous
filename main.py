377b39bbb5eb691a97f248545645b792fbec5bf9
Ale Tourn <rubentourn2000@gmail.com>
2023-03-25 19:33:55 -0300

    colores, quito comments, más

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# Python Turtle - Lissajous Curve - www.101computing.net/python-turtle-lissajous-curve/
 import turtle
+import random
 from math import cos, sin
 
 
 def ask_values():
-    a = int(input("Dígame a: "))
-    b = int(input("Dígame b: "))
-    delta = float(input("Dígame delta mayor que 0: "))
+    a = int(input("Introduzca un valor a: "))
+    b = int(input("Introduzca un valor b: "))
+    delta = float(input("Introduzca un valor delta mayor que 0: "))
     delta = 3.14 / delta
-    t = int(input("Dígame t: "))
+    t = int(input("Introduzca un valor t: "))
     return a, b, delta, t
 
 
@@ -20,19 +20,19 @@ def draw_line(a, b, delta, t):
     myPen.hideturtle()
     myPen.pensize(1)
     myPen.color("#ffffff")
+    colors = ["#ffcccc", "#ffd9b3", "#ffffcc", "#c1ffc1", "#cce6ff", "#e6e6fa", "#f5e6ea"]
 
     while delta < 10:
         myPen.clear()
-        # myPen.penup()
-        delta += 0.02
-        for i in range(0, 700):
-            t += 0.01
-            # Apply Lissajous Parametric Equations
-            x = 100 * sin(a * t + delta)
-            y = 100 * sin(b * t)
+        delta += 0.025
+        myPen.color(random.choice(colors))
+        for i in range(0, 500):
+            t += 0.06
+            
+            x = 225 * sin(a * t + delta)
+            y = 225 * sin(b * t)
 
             myPen.goto(x, y)
-            # myPen.pendown()
             myPen.getscreen().update()
     turtle.done()
 
